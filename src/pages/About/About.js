339fad9c330ebc12a4146dc5d65678dd65aae6cd
Dimterion <dimterion59@gmail.com>
2022-09-10 10:32:07 +0200

    Added About page content.

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,55 @@
+import image from "../../assets/image_park.jpg";
+import "./about.css";
+
 function About() {
   return (
-    <h1>
-      WORK IN PROGRESS. But you can check the links at the bottom of the
-      page. Thanks!
-    </h1>
+    <div className="aboutPage-container">
+      <img className="aboutPage-image" src={image} alt="Park in Paris" />
+      <h2>Hi! I'm Dmitrii</h2>
+      <p>
+        I live in France and learn Web Development focusing on Front-End (with
+        some bits of Back-End).
+      </p>
+      <p>
+        This small site was made in order to learn React using its oficial
+        documentation. I'm mostly focusing on testing some of the techniques
+        rather than making everything ideal.
+      </p>
+      <h3>Contact links</h3>
+      <ul className="aboutPage-list">
+        <li>
+          <a
+            className="link"
+            href="https://github.com/Dimterion"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Here is my GitHub profile
+          </a>
+        </li>
+        <li>
+          <a
+            className="link"
+            href="https://twitter.com/Dimterion"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            And this is my Twitter
+          </a>
+        </li>
+        <li>
+          <a
+            className="link"
+            href="https://medium.com/@dimterion"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            I also write stories on Medium every Friday
+          </a>
+        </li>
+      </ul>
+      <i>Fan fact: avatar at the top left corner was made using CSS</i>
+    </div>
   );
 }
 
