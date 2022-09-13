7e0ceecbb474e194e7197d923189d8e54745c2da
Dimterion <dimterion59@gmail.com>
2022-09-13 12:26:46 +0200

    Added styling and interactivity to the quiz.

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import image from "../../assets/image_cliffs.jpg";
 import "./quiz.css";
 
 function Quiz() {
@@ -7,7 +8,20 @@ function Quiz() {
   const [status, setStatus] = useState("typing");
 
   if (status === "success") {
-    return <h1>Good job on those geography skills!</h1>;
+    return (
+      <div className="quiz-container">
+        <h3>Good job on those geography skills!</h3>
+        <p>Here is another picture for you.</p>
+        <img
+          className="quiz-image"
+          src={image}
+          alt="Cliffs and sea at the north of France"
+        />
+        <button className="quiz-button" onClick={handleReset}>
+          Reset
+        </button>
+      </div>
+    );
   }
 
   async function handleSubmit(e) {
@@ -26,18 +40,30 @@ function Quiz() {
     setAnswer(e.target.value);
   }
 
+  function handleReset() {
+    setStatus("typing");
+    setAnswer("");
+  }
+
   return (
     <>
       <h3>You do know where Normandy is, right?</h3>
+      <i>
+        Type the country name. Just letters (capital or not doesn't matter).
+      </i>
       <form onSubmit={handleSubmit}>
         <textarea
+          className="quiz-textarea"
           value={answer}
           onChange={handleTextareaChange}
           disabled={status === "submitting"}
         />
         <br />
-        <button disabled={answer.length === 0 || status === "submitting"}>
-          Submit
+        <button
+          className="quiz-button"
+          disabled={answer.length === 0 || status === "submitting"}
+        >
+          Of course I know!
         </button>
         {error !== null && <p>{error.message}</p>}
       </form>
@@ -50,7 +76,11 @@ function submitForm(answer) {
     setTimeout(() => {
       let shouldError = answer.toLowerCase() !== "france";
       if (shouldError) {
-        reject(new Error("Oh no! That's not correct. Try again (hint: it starts with F)."));
+        reject(
+          new Error(
+            "Oh no! That's not correct. Try again (hint: it starts with F)."
+          )
+        );
       } else {
         resolve();
       }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Quiz from "../../components/Quiz/Quiz";
 import image from "../../assets/image_sea.jpg";
 import "./home.css";
 
 function Home() {
+  const [quiz, setQuiz] = useState(false);
+
+  function handleQuiz() {
+    setQuiz(!quiz);
+  }
+
   return (
     <div className="home-container">
       <img
@@ -23,7 +30,9 @@ function Home() {
         in the appropriate sections. Meanwhile, take a look at some of the
         Normandy's beauties.
       </p>
-      <Quiz />
+      <h4>Want to see a tiny quiz?</h4>
+      <button className="quiz-button" onClick={handleQuiz}>{!quiz ? "Sure" : "Changed my mind"}</button>
+      {quiz && <Quiz />}
     </div>
   );
 }
