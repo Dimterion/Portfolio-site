ec10c443650732343c729316c13fc29b6740a7ee
Dimterion <dimterion59@gmail.com>
2022-10-22 11:21:23 +0200

    Added back btn for the tasks page.

--- src/pages/Tasks/Tasks.js
+++ src/pages/Tasks/Tasks.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import TasksDropdown from "../../components/TasksDropdown/TasksDropdown";
 import "./tasks.css";
 
@@ -6,6 +7,9 @@ function Tasks() {
     <>
       <h2 className="tasks-header">A simple To Do app</h2>
       <TasksDropdown />
+      <Link className="tasks-link" to="/projects">
+        Back to the projects page
+      </Link>
     </>
   );
 }
