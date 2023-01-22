4d9cd2bc2b6e518a7260ffc644525c7735060212
Dimterion <dimterion59@gmail.com>
2023-01-22 10:37:03 +0100

    Minor styling and added link to JSChallenges page.

--- src/components/JSChallengesTextForm/JSChallengesTextForm.jsx
+++ src/components/JSChallengesTextForm/JSChallengesTextForm.jsx
@@ -7,69 +7,77 @@ function JSChallengesTextForm() {
 
   return (
     <div className="jsChallenges-container">
-      <h3>Text Edit Form</h3>
-      <h4>A simple form to modify text input</h4>
-      <form>
-        <textarea
-          className="jsChallenges-text-input"
-          value={text}
-          onChange={(e) => {
-            setText(e.target.value);
-          }}
-        />
-        <div className="jsChallenges-buttons-container">
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              setText(text.toUpperCase());
+      <div>
+        <h3>Text Edit Form</h3>
+        <h4>A simple form to modify text input</h4>
+        <form>
+          <textarea
+            className="jsChallenges-text-input"
+            value={text}
+            onChange={(e) => {
+              setText(e.target.value);
             }}
-          >
-            Uppercase
-          </button>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              setText(text.toLowerCase());
-            }}
-          >
-            Lowercase
-          </button>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              capEveryOtherChar(text, setText);
-            }}
-          >
-            Capitalize every other character
-          </button>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              removeDup(text, setText);
-            }}
-          >
-            Remove duplicate characters
-          </button>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              scream(text, setText);
-            }}
-          >
-            Scream
-          </button>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              setText("");
-            }}
-          >
-            Clear text
-          </button>
-        </div>
-      </form>
-      <br></br>
-      <br></br>
+          />
+          <div className="jsChallenges-buttons-container">
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                setText(text.toUpperCase());
+              }}
+            >
+              Uppercase
+            </button>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                setText(text.toLowerCase());
+              }}
+            >
+              Lowercase
+            </button>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                capEveryOtherChar(text, setText);
+              }}
+            >
+              Capitalize every other character
+            </button>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                removeDup(text, setText);
+              }}
+            >
+              Remove duplicate characters
+            </button>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                scream(text, setText);
+              }}
+            >
+              Scream
+            </button>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                setText("");
+              }}
+            >
+              Clear text
+            </button>
+          </div>
+        </form>
+      </div>
+      <a
+        className="link"
+        href="https://medium.com/@dimterion/practical-usage-of-coding-challenges-4fbbd314311f"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Description
+      </a>
     </div>
   );
 }
