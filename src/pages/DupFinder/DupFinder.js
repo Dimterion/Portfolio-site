0b5d4654baef1ae53f0d6a923784ff772d01ca09
Dimterion <dimterion59@gmail.com>
2023-03-16 16:47:29 +0100

    add comments, minor fixes

--- src/pages/DupFinder/DupFinder.js
+++ src/pages/DupFinder/DupFinder.js
@@ -2,26 +2,38 @@ import { useState } from "react";
 import "./dupFinder.css";
 
 function DupFinder() {
+  // Text to check for duplicate words
   const [text, setText] = useState("");
+
+  // Array with duplicate words to display
   const [dup, setDup] = useState([]);
 
   function findDup(text, setDup) {
-    let words = text.toLowerCase().split(" ");
+    // Removing all punctuation from the text
+    const textWithNoPunctuation = text
+      .replace(/[.,/#!?$%^&*;:{}=\-_`~()]/g, "")
+      .replace(/\s{2,}/g, " ");
 
+    // Setting each text word to lower case and splitting the text to get an array of words
+    const words = textWithNoPunctuation.toLowerCase().split(" ");
+
+    // Object to store duplicate words and their count
     const dups = {};
 
+    // Checking for whitespaces and adding each word and its count into the dups object
     words.forEach((word) => {
-      dups[word] = (dups[word] || 0) + 1;
+      word !== "" && (dups[word] = (dups[word] || 0) + 1);
     });
 
+    // Final array for duplicates and their count
     let wordsCount = [];
 
+    // Pushing all duplicates into the array
     for (const word in dups) {
-      word !== "" &&
-        dups[word] > 1 &&
-        wordsCount.push(`${word}: ${dups[word]}`);
+      dups[word] > 1 && wordsCount.push(`${word}: ${dups[word]}`);
     }
 
+    // Setting the array with duplicates to display on the page
     setDup(wordsCount);
   }
 
@@ -46,6 +58,7 @@ function DupFinder() {
         </button>
       </form>
       <ul>
+        {/* Mapping through the duplicates array to display the words from it and their count */}
         {dup.map((word) => (
           <li key={`${word}-${dup.indexOf(word)}`}>{word}</li>
         ))}
